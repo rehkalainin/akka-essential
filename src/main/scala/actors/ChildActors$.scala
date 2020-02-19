54cd3bd26fb52ad12c31f1cdc6a7b450db552795
rehkalainin <rkonstantin@gmail.com>
2020-02-19 16:57:56 +0200

    distributed actor work

---
+++ src/main/scala/actors/ChildActors$.scala
@@ -0,0 +1,37 @@
+package actors
+
+import actors.ChildActors.Parent.{CreateActor, TellChild}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+object ChildActors extends App{
+
+// Actor can crate other actor
+  object Parent{
+  case class CreateActor(name:String)
+  case class TellChild(message:String)
+}
+  class Parent extends Actor{
+    override def receive: Receive = {
+      case CreateActor(name)=>
+        println(s"${self.path} Create actor $name")
+        val childRef = context.actorOf(Props[Child], s"$name")
+        context.become(withChild(childRef))
+    }
+    def withChild(childRef: ActorRef):Receive={
+      case TellChild(message)=> childRef ! message
+    }
+  }
+
+
+  class Child extends Actor{
+    override def receive: Receive = {
+      case message=> println(s"${self.path} I got message : $message")
+    }
+  }
+  val system = ActorSystem("ChildActorSystem")
+  val parent = system.actorOf(Props[Parent],"parent")
+
+  parent ! CreateActor("myActorChild")
+  parent ! TellChild("Hello my child actor")
+
+}
---
+++ src/main/scala/exersices/ChildActorExersice.scala
@@ -0,0 +1,64 @@
+package exersices
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+object ChildActorExersice extends App {
+
+  object WordCounterMaster{
+    case class Initialize(nChildren:Int)
+    case class WordCountTask(tackId:Int, text:String)
+    case class WordCountReply(id:Int, counter:Int)
+  }
+  class WordCounterMaster extends Actor{
+    import WordCounterMaster._
+
+    override def receive: Receive = {
+      case Initialize(n)=>
+        println(s"[master] Initializing ... ")
+        val childrenRefs = for (i<- 1 to n) yield context.actorOf(Props[WordCounterWorker],s"wcw_$i")
+        context.become(withChildren(childrenRefs,0,0,Map()))
+    }
+
+    def withChildren(childrenRefs: Seq[ActorRef], currentChildrenIndex: Int, currentTaskId: Int, requestMap: Map[Int, ActorRef]):Receive={
+      case text:String =>
+        println(s"[master] I have receive : $text - I will send it to child $currentChildrenIndex")
+        val originalSender = sender()
+        val childRef = childrenRefs(currentChildrenIndex)
+        childRef ! WordCountTask(currentTaskId,text)
+        val nextChildIndex = (currentChildrenIndex+1)% childrenRefs.length
+        val newTaskId = currentTaskId+1
+        val newRequesMap = requestMap+ (currentTaskId-> originalSender)
+        context.become(withChildren(childrenRefs,nextChildIndex,newTaskId,newRequesMap))
+
+      case WordCountReply(id, counter)=>
+        println(s"[master] I have received Reply for tast $id with $counter")
+        val originalSender = requestMap(id)
+        originalSender ! counter
+        context.become(withChildren(childrenRefs,currentChildrenIndex,currentTaskId,requestMap-id))
+
+    }
+  }
+
+  class WordCounterWorker extends Actor{
+    import exersices.ChildActorExersice.WordCounterMaster._
+    override def receive: Receive = {
+      case WordCountTask(id,task)=> sender() ! WordCountReply(id, task.split(" ").length)
+    }
+  }
+
+  class TestActor extends Actor{
+    import WordCounterMaster._
+    override def receive: Receive = {
+      case "go"=>
+        val master = context.actorOf(Props[WordCounterMaster],"master")
+        master ! Initialize(3)
+        val texts = List("I love akka", "Scala is awesome", "yes", "me too")
+        texts.map(text=> master ! text)
+      case count =>
+        println(s"[test actor] I received a reply: $count")
+    }
+  }
+  val system = ActorSystem("mySystem")
+  val testActor = system.actorOf(Props[TestActor],"testActor")
+  testActor ! "go"
+}
